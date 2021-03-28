a6b9975532763073ac3eb0d7e89f14815e72b4e1
Adam Hall <adamhh@uw.edu>
2021-03-27 21:08:56 -0700

    slider and arrows working, now to look into resizing

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,12 +7,15 @@ import "slick-carousel/slick/slick-theme.css";
 function ImageSlider() {
     let settings = {
         dots: true,
+        arrows: true,
         infinite: true,
+        centerMode: false,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
         cssEase: "linear"
     }
+
     return (
         <Slider {...settings}>
             <div className="card-wrapper">
